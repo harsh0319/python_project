1200c72766d505413aa3cb893514a0acab1e4f04
harsh0319 <78340872+harsh0319@users.noreply.github.com>
2022-07-24 14:31:21 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,26 @@
+from http.server import executable
+import os
+import cx_Freeze
+import sys
+import os
+from importlib_metadata import version
+
+from requests import options
+base = None
+
+if sys.platform == 'win32':
+    base= "Win32GUI"
+
+os.environ['TCL_LIBRARY'] = r"C:\Program Files\Python39\tcl\tcl8.6" 
+os.environ['TK_LIBRARY'] = r"C:\Program Files\Python39\tcl\tk8.6"
+
+executables = [cx_Freeze.Executable("project_1.py", base=base, icon="icon.ico")]
+
+
+cx_Freeze.setup(
+    name= "Text Editor",
+    options = {"build_exe": {"packages":["tkinter","os"], "include_files":["icon.ico","tcl86t.dll","tk86t.dll", 'icons2']}},
+    version="0.01",
+    description = "Tkinter Application",
+    executables = executables
+)
